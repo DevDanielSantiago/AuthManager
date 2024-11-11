934f628e86716d4497aaf701c978deeb75a9e330
DevDanielSantiago <dev.danielsantiago@gmail.com>
2024-11-11 14:04:16 -0400

    feat: implement list service from permission service

--- src/permission/service/permission.service.ts
+++ src/permission/service/permission.service.ts
@@ -2,7 +2,13 @@ import { Injectable } from '@nestjs/common';
 
 import { ConflictException } from 'src/exception';
 
-import { PermissionDto, MessageResponseDto } from 'src/permission/dto';
+import { ListResponseDto, MessageResponseDto } from 'src/dto';
+import {
+  PermissionDto,
+  ResponsePermissionDto,
+  HeadersPermissionDto,
+} from 'src/permission/dto';
+
 import { PermissionRepository } from 'src/permission/repository/permission.repository';
 
 @Injectable()
@@ -20,5 +26,22 @@ export class PermissionService {
 
     await this.permissionRepository.create(permission);
     return { message: 'Permission created sucessfully' };
+  }
+
+  async list(
+    headers: HeadersPermissionDto,
+  ): Promise<ListResponseDto<ResponsePermissionDto>> {
+    const page = parseInt(headers['x-page'], 10) || 1;
+    const limit = parseInt(headers['x-limit'], 10) || 10;
+
+    const filterName = headers['x-filter-name'];
+
+    const filters: Record<string, RegExp> = {};
+    if (filterName) filters['name'] = new RegExp(filterName, 'i');
+
+    const list = await this.permissionRepository.find(filters, page, limit);
+    const count = await this.permissionRepository.count(filters);
+
+    return { count, list };
   }
 }
