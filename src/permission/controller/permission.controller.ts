3473a7b52fc5785409264ed0cbe1dc2b31829514
DevDanielSantiago <dev.danielsantiago@gmail.com>
2024-11-11 14:04:58 -0400

    feat: implement list controller from permission controller

--- src/permission/controller/permission.controller.ts
+++ src/permission/controller/permission.controller.ts
@@ -1,11 +1,24 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 
-import { PermissionDto } from 'src/permission/dto';
+import { HeadersPermissionDto, PermissionDto } from 'src/permission/dto';
 import { PermissionService } from 'src/permission/service/permission.service';
 
 @Controller('permission')
 export class PermissionController {
   constructor(private readonly permissionService: PermissionService) {}
+
+  @Get()
+  async index(@Headers() headers: HeadersPermissionDto) {
+    return this.permissionService.list(headers);
+  }
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
