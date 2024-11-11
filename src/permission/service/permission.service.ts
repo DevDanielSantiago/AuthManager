d4c2b83fcb5f652fcc4690ce345aeee10b88b16a
DevDanielSantiago <dev.danielsantiago@gmail.com>
2024-11-10 21:51:42 -0400

    feat: create permission service with create method

---
+++ src/permission/service/permission.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+
+import { ConflictException } from 'src/exception';
+
+import { PermissionDto, MessageResponseDto } from 'src/permission/dto';
+import { PermissionRepository } from 'src/permission/repository/permission.repository';
+
+@Injectable()
+export class PermissionService {
+  constructor(private readonly permissionRepository: PermissionRepository) {}
+
+  async create(permission: PermissionDto): Promise<MessageResponseDto> {
+    const findPermission = await this.permissionRepository.findOne({
+      name: permission.name,
+    });
+
+    if (findPermission)
+      throw new ConflictException('Permission already exists');
+
+    await this.permissionRepository.create(permission);
+    return { message: 'Permission created sucessfully' };
+  }
+}
