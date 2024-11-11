c1d356caf52ac1c9b534be4e29df21064d8356e2
DevDanielSantiago <dev.danielsantiago@gmail.com>
2024-11-11 08:55:48 -0400

    fix: adjust description data type error message

--- src/permission/dto/permission.dto.ts
+++ src/permission/dto/permission.dto.ts
@@ -9,7 +9,7 @@ export class PermissionDto {
   @MaxLength(20, { message: 'Permission name cannot exceed 20 characters' })
   name: string;
 
-  @IsString({ message: 'Permission name is string' })
+  @IsString({ message: 'Permission description is string' })
   @IsNotEmpty({ message: 'Description is required' })
   @MinLength(10, { message: 'Description must be at least 10 characters long' })
   @MaxLength(100, { message: 'Description cannot exceed 100 characters' })
