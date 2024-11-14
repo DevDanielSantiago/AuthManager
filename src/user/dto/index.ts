1d5bf3e7671464f2b6731a26a60621c85d5c1f03
DevDanielSantiago <dev.danielsantiago@gmail.com>
2024-11-14 17:14:20 -0400

    feat: export DTO's

--- src/user/dto/index.ts
+++ src/user/dto/index.ts
@@ -3,3 +3,6 @@ export * from './createUser.dto';
 export * from './updateUser.dto';
 export * from './headersUser.dto';
 export * from './updateUserPassword.dto';
+export * from './updateUserEmail.dto';
+export * from './responseEmailChangeRequest.dto';
+export * from './emailChangeRequest.dto';
