3dfacec41591b4b6c57684155614aae89b9454b9
DevDanielSantiago <dev.danielsantiago@gmail.com>
2024-11-14 18:53:07 -0400

    feat: initial implementation to the method email update

--- src/user/service/user.service.ts
+++ src/user/service/user.service.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import bcrypt from 'bcrypt';
+import { v4 as uuid } from 'uuid';
 
-import { UserRepository } from '../repository/user.repository';
+import { UserRepository, EmailChangeRequestRepository } from '../repository';
 import {
   CreateUserDto,
   HeadersUserDto,
   ResponseUserDto,
   UpdateUserDto,
+  UpdateUserEmailDto,
   UpdateUserPasswordDto,
 } from '../dto';
 import { ListResponseDto, MessageResponseDto } from 'src/dto';
 import { BadRequestException, ConflictException } from 'src/exception';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly emailChangeRequestRepository: EmailChangeRequestRepository,
+    private readonly mailerService: MailerService,
+  ) {}
 
   async list(
     headers: HeadersUserDto,
@@ -86,5 +93,38 @@ export class UserService {
 
     await this.userRepository.update(id, { password: hashedPassword });
     return { message: 'Password updated sucessfully' };
+  }
+
+  async updateEmail(
+    id: string,
+    user: UpdateUserEmailDto,
+  ): Promise<MessageResponseDto> {
+    const findUser = await this.userRepository.findOne({ _id: id });
+    if (!findUser) throw new ConflictException("User doesn't exists");
+
+    const isPasswordValid = await bcrypt.compare(
+      user.password,
+      findUser.password,
+    );
+    if (!isPasswordValid)
+      throw new BadRequestException('Current password is incorrect');
+
+    const token = uuid();
+    const tokenExpiration = new Date(Date.now() + 3600 * 1000);
+
+    const requestResult = await this.emailChangeRequestRepository.create({
+      userId: id,
+      newEmail: user.email,
+      token,
+      tokenExpiration,
+    });
+
+    await this.mailerService.sendMail({
+      to: findUser.email,
+      subject: 'Atualização de e-mail',
+      text: `ID de recuperação: ${requestResult._id}`,
+    });
+
+    return { message: 'Request created sucessfully' };
   }
 }
