4a94e53e17b1d16cdf7a9d4a0fc7eb98b8c64763
DevDanielSantiago <dev.danielsantiago@gmail.com>
2024-11-18 20:38:26 -0400

    feat: create list user method at userController

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Headers } from '@nestjs/common';
+import { UserService } from '../service/user.service';
+import { HeadersUserDto } from '../dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async index(@Headers() headers: HeadersUserDto) {
+    return this.userService.list(headers);
+  }
+}
