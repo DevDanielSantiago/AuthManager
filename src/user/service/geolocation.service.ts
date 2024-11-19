0edeb3daff50d80ad61ff2a9a60a62b0397cd9bd
DevDanielSantiago <dev.danielsantiago@gmail.com>
2024-11-19 19:17:27 -0400

    refactor: add type response from getGeoLocation request

--- src/user/service/geolocation.service.ts
+++ src/user/service/geolocation.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@nestjs/common';
 
 import axios from 'axios';
 
+import { IPAddressDto } from 'src/dto';
+
 @Injectable()
 export class GeoLocationService {
   private readonly apiKey = process.env.IPSTACK_API_KEY;
 
   async getGeoLocation(ip: string) {
     const url = `http://api.ipstack.com/${ip}?access_key=${this.apiKey}`;
-    const response = await axios.get(url);
+    const response = await axios.get<IPAddressDto>(url);
     return response.data;
   }
 }
