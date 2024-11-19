f5bdda313216a79b2a8e9d83988ec8aab6b435fb
DevDanielSantiago <dev.danielsantiago@gmail.com>
2024-11-19 19:22:39 -0400

    refactor: update validation message from newPassword on UpdateUserPasswordDto

--- src/user/dto/updateUserPassword.dto.ts
+++ src/user/dto/updateUserPassword.dto.ts
@@ -6,15 +6,15 @@ export class UpdateUserPasswordDto {
   currentPassword: string;
 
   @IsString()
-  @IsNotEmpty({ message: 'User password is required' })
+  @IsNotEmpty({ message: 'User newPassword is required' })
   @Length(8, 20, {
-    message: 'User password must be between 8 and 20 characters long',
+    message: 'User newPassword must be between 8 and 20 characters long',
   })
   @Matches(
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()])[A-Za-z\d!@#$%^&*()]+$/,
     {
       message:
-        'User password must contain at least one lowercase letter, one uppercase letter, one number, and one special character (!@#$%^&*())',
+        'User newPassword must contain at least one lowercase letter, one uppercase letter, one number, and one special character (!@#$%^&*())',
     },
   )
   newPassword: string;
