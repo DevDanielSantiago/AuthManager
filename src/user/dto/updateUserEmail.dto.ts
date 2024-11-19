568e730c939293da55befe7375bcbbcbc1669c45
DevDanielSantiago <dev.danielsantiago@gmail.com>
2024-11-19 19:03:58 -0400

    refactor: remove lengh and match validation from UpdateUserEmailDto

--- src/user/dto/updateUserEmail.dto.ts
+++ src/user/dto/updateUserEmail.dto.ts
@@ -1,10 +1,4 @@
-import {
-  IsEmail,
-  IsNotEmpty,
-  IsString,
-  Length,
-  Matches,
-} from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdateUserEmailDto {
   @IsEmail({}, { message: 'User email must be a valid email address' })
@@ -13,15 +7,5 @@ export class UpdateUserEmailDto {
 
   @IsString()
   @IsNotEmpty({ message: 'User password is required' })
-  @Length(8, 20, {
-    message: 'User password must be between 8 and 20 characters long',
-  })
-  @Matches(
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()])[A-Za-z\d!@#$%^&*()]+$/,
-    {
-      message:
-        'User password must contain at least one lowercase letter, one uppercase letter, one number, and one special character (!@#$%^&*())',
-    },
-  )
   password: string;
 }
