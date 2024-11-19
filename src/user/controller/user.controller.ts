9b73851f31dee7ed889b2100b2ee45702bbbf64e
DevDanielSantiago <dev.danielsantiago@gmail.com>
2024-11-18 20:40:42 -0400

    feat: create add user method at userController

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,6 +1,14 @@
-import { Controller, Get, Headers } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { UserService } from '../service/user.service';
-import { HeadersUserDto } from '../dto';
+import { CreateUserDto, HeadersUserDto } from '../dto';
 
 @Controller('user')
 export class UserController {
@@ -9,5 +17,11 @@ export class UserController {
   @Get()
   async index(@Headers() headers: HeadersUserDto) {
     return this.userService.list(headers);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
   }
 }
