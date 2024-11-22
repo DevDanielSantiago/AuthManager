548c79f2badd4b71a2fc7acd65e3e6ed4f0c1932
Daniel Santiago <dev.danielsantiago@gmail.com>
2024-11-21 20:11:55 -0400

    feat: create Forbidden expection

--- src/common/exception/index.ts
+++ src/common/exception/index.ts
@@ -2,3 +2,4 @@ export * from './not-found.exception';
 export * from './conflict.exception';
 export * from './bad-request.exception';
 export * from './unauthorized.exception';
+export * from './forbidden.exception';
